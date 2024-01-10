640fc2f7a34f7745c5236362069e60a9051a53eb
Sabhi Sharma <2021is_sabhisharma_b@nie.ac.in>
2024-01-11 02:00:31 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Music Recommendation App
+
+Welcome to the Music Recommendation App! This Node.js and Express project allows users to discover new songs based on their preferred genre and artist.
+
+## Project Structure
+
+- `app.js`: Server file containing the main logic for handling HTTP requests, integrating with the MySQL Database, and serving static files.
+- `public/`: Folder containing static files such as images, CSS, and JS files.
+- `views/`: Folder containing ejs files
+    - `index.ejs`: JavaScript templating language for generating HTML with plain JavaScript. 
+- `MUCE.html`: Main HTML file for the application home page.
+
+## Features
+
+- User Input: Users can specify their preferred genre and artist to get personalized music recommendations.
+- Database: The app uses a MySQL database to store and retrieve song information.
+- Responsive Design: The application is designed to work on various devices.
---
+++ app.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const axios = require('axios');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+const db_user = process.env.DB_User;
+const db_pass = process.env.DB_Pass;
+const db_project = process.env.DB_Project;
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: db_user,
+  password: db_pass,
+  database: db_project
+});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static( __dirname + "/public/" ));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/MUCE.html');
+});
+
+app.get('/getRecommendations', (req, res) => {
+  res.render('personalizedMusic');
+});
+
+app.post('/getRecommendations', (req, res) => {
+  const inputGenre = req.body.genre;
+  const inputArtist = req.body.artist;
+
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+    // SQL Query
+    con.query(`SELECT title AS Title, artist AS Artist, genre AS Genre, release_date AS 'Release Date', duration AS Duration, album AS Album, 
+    spotify_link AS Spotify FROM songs WHERE artist = '${inputArtist}' AND genre = '${inputGenre}';`, async function (err, result, fields) {
+      if (err) throw err;
+
+      // Function to manipulate Spotify link to resemble the embed link
+      // const link = result[0].Spotify;
+      // const parts = link.split('?si=');
+      // const baseLink = parts[0];
+      // const queryParameters = parts[1];
+      // const newLink = `${baseLink.replace('/track/', '/embed/track/')}` + `?utm_source=generator`;
+
+      // console.log(newLink);
+
+      // Check if there is at least one result
+      if (result.length > 0) {
+
+        // Create the recommendations array
+        const recommendations = result.map(song => {
+          // Function to manipulate Spotify link to resemble the embed link
+          const link = song.Spotify;
+          const parts = link.split('?si=');
+          const baseLink = parts[0];
+          const queryParameters = parts[1];
+          const newLink = `${baseLink.replace('/track/', '/embed/track/')}` + `?utm_source=generator`;
+
+          return {
+            Title: song.Title,
+            Artist: song.Artist,
+            Genre: song.Genre,
+            'Release Date': song['Release Date'],
+            Duration: song.Duration,
+            Album: song.Album,
+            Spotify: song.Spotify,
+            'Spotify Embed': newLink
+          };
+        });
+
+        // Create the recommendations object
+        // const recommendations = [
+        //   {
+        //     Title: result[0].Title,
+        //     Artist: result[0].Artist,
+        //     Genre: result[0].Genre,
+        //     'Release Date': result[0]['Release Date'],
+        //     Duration: result[0].Duration,
+        //     Album: result[0].Album,
+        //     Spotify: result[0].Spotify,
+        //     'Spotify Embed': newLink
+        //   },
+        // ];
+
+        // Render the recommendations.ejs page and pass the recommendations data
+        res.render('recommendations', { recommendations });
+      } else {
+        // No recommendations found
+        res.render('recommendations', { recommendations: [] });
+      }
+    });
+
+    // Reconnect in order to prevent the error: "Cannot enqueue Handshake after invoking quit"
+    con.end(function (err) {
+      if (err) { console.log("Error ending the connection:", err); }
+      con = mysql.createConnection({
+        host: "localhost",
+        user: db_user,
+        password: db_pass,
+        database: db_project
+      });
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
